729335977a363c8a8460f4e8ef59ff6b1929ab5c
Martin Miranda <martinmirandac@gmail.com>
2021-03-17 15:01:13 +1100

    emojis :)

--- seeds/01_lists.js
+++ seeds/01_lists.js
@@ -3,9 +3,9 @@ exports.seed = function(knex) {
   return knex('lists').del()
     .then(function () {
       return knex('lists').insert([
-        {id: 1, name: 'Life'},
-        {id: 2, name: 'Work'},
-        {id: 3, name: 'Learning'}
+        {id: 1, name: '🌳 Life'},
+        {id: 2, name: '💼 Work'},
+        {id: 3, name: '🤓 Learning'}
       ])
       .then(() => {
         return knex.raw('SELECT setval(\'lists_id_seq\',(SELECT MAX(id) from "lists"))')
