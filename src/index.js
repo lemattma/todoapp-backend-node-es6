51cc0a0cb815bdb3f8db6e261d11ef0212a4f2b3
Martin Miranda <martinmirandac@gmail.com>
2021-03-20 10:37:07 +1100

    delete lists and tasks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 
+const knexfile = require('../knexfile');
+const knex = require("knex")(knexfile);
+const bookshelf = require('bookshelf')(knex);
+
 const List = require('./models/list')
 const Task = require('./models/task');
 
@@ -30,6 +34,21 @@ app.get("/lists/:id", (req, res) =>
     )
 );
 
+app.delete("/lists/:id", (req, res) =>
+    List.forge({id: req.params.id}).fetch()
+        .catch(() => res.status(404).json())
+        .then(list => {
+            bookshelf.transaction((t) => {
+                return Task.where({list_id: req.params.id}).destroy({transacting: t, require: false})
+                    .then(() => {
+                        return list.destroy({transacting: t})
+                    })
+            })
+            .then(() =>  res.status(203).json())
+            .catch(err => res.status(500).json({ error: err }))
+    })
+);
+
 app.post("/lists", (req, res) =>
     List.forge({ name: req.body.name }).save()
         .then(list => res.status(201).json(list))
@@ -45,6 +64,16 @@ app.post("/tasks", (req, res) =>
     }).save()
         .then(task => res.status(201).json(task))
         .catch(err => res.status(400).json({error: err}))
+);
+
+app.delete("/tasks/:id", (req, res) =>
+    Task.forge({id: req.params.id}).fetch()
+        .catch(() => res.status(404).json())
+        .then(task => {
+            task.destroy();
+            res.status(203).json();
+        })
+        .catch(err => res.status(500).json({ error: err }))
 );
 
 app.listen(process.env.PORT, () => console.log(`Server is running`));
